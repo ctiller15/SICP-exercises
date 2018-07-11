5a6b64caf02f7a7c6335389c62cb560793c39505
ctiller15 <christiller2015@gmail.com>
2018-07-10 20:27:05 -0400

    -Add in fibonacci exercises.

---
+++ Fibonacci-exercises.rkt
@@ -0,0 +1,44 @@
+#lang racket
+; A recursive Fibonacci sequence.
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+(fib 7) ; 13
+
+; An iterative Fibonacci.
+
+(define (iterative-fib n)
+  (fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+
+(iterative-fib 7) ; Also 13.
+
+;A procedure that solves the 'counting change' problem.
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+(count-change 100)
+
