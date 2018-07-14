3101ac7f0b77ee510fcb8cd6385da25ed207e15c
ctiller15 <christiller2015@gmail.com>
2018-07-13 20:19:27 -0400

    Create iterative exponentiation of log n complexity

---
+++ Ex1.16.rkt
@@ -0,0 +1,23 @@
+#lang racket
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(even? 10)
+
+(define (sq n)
+  (* n n))
+
+(sq 5)
+
+(define (** b n)
+  (**-iter b n 1))
+
+(define (**-iter b n a)
+  (writeln a)
+  (writeln b)
+  (writeln n)
+  (cond ((= n 1) a)
+        ((even? n) (**-iter b (/ n 2) (* a (sq b))))
+        ( else (**-iter b (- n 1) (* a b)))))
+
+(** 5 4)
