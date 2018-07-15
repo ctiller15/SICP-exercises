812cf5ca9de11a835b4e70b22fb6e797a08869ed
ctiller15 <christiller2015@gmail.com>
2018-07-14 22:32:06 -0400

    Complete ex 1.19, using the russian peasant method to calculate fibonacci in a logarithmic number of steps.

---
+++ Russian-peasant-fibonacci.rkt
@@ -0,0 +1,29 @@
+#lang racket
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+#| By using this method to compute fibonacci, we have a way to explicitly square
+The transformation.
+As such, we can create a procedure that runs
+In a logarithmic number of steps.
+|#
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (+ (* p p) (* q q))
+                   (+ (* 2 p q) (* q q))
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+;0 1 1 2 3 5 8 13 21
+(fib 0) ; 0
+(fib 1) ; 1
+(fib 5) ; 5
+(fib 6) ; 8
+(fib 7) ; 13
+(fib 8) ; 21
